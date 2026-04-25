73b57e07bd90adf9351229715fc680ad57223a6e
Dmitrii Zhukov <zhukov.d@gologin.com>
2026-04-25 11:44:56 +0700

    test(metrics): cover last_duration, last_failure, next_expected, running_jobs

--- internal/metrics/collector_test.go
+++ internal/metrics/collector_test.go
@@ -96,3 +96,49 @@ cronguard_condition{cronjob="demo-cj",name="demo",namespace="ns1",reason="Consec
 		t.Fatal(err)
 	}
 }
+
+func TestCollectorEmitsRemainingMetrics(t *testing.T) {
+	failure := metav1.NewTime(time.Date(2026, 4, 24, 1, 0, 0, 0, time.UTC))
+	next := metav1.NewTime(time.Date(2026, 4, 24, 4, 0, 0, 0, time.UTC))
+	dur := int32(120)
+	cjm := monitoringv1alpha1.CronJobMonitor{
+		ObjectMeta: metav1.ObjectMeta{Name: "demo", Namespace: "ns1"},
+		Spec:       monitoringv1alpha1.CronJobMonitorSpec{CronJobRef: monitoringv1alpha1.CronJobReference{Name: "demo-cj"}},
+		Status: monitoringv1alpha1.CronJobMonitorStatus{
+			LastFailureTime:  &failure,
+			NextExpectedTime: &next,
+			RecentExecutions: []monitoringv1alpha1.ExecutionRecord{
+				{JobName: "newest", StartTime: metav1.Now(), Phase: monitoringv1alpha1.ExecutionPhaseRunning},
+				{JobName: "older", StartTime: metav1.Now(), DurationSeconds: &dur, Phase: monitoringv1alpha1.ExecutionPhaseSucceeded},
+			},
+		},
+	}
+
+	c := metrics.NewCollector(stubLister{items: []monitoringv1alpha1.CronJobMonitor{cjm}})
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(c)
+
+	expected := fmt.Sprintf(`
+# HELP cronguard_last_duration_seconds Duration of last completed Job in seconds
+# TYPE cronguard_last_duration_seconds gauge
+cronguard_last_duration_seconds{cronjob="demo-cj",name="demo",namespace="ns1"} 120
+# HELP cronguard_last_failure_timestamp_seconds Unix time of last failed Job; 0 if never
+# TYPE cronguard_last_failure_timestamp_seconds gauge
+cronguard_last_failure_timestamp_seconds{cronjob="demo-cj",name="demo",namespace="ns1"} %d
+# HELP cronguard_next_expected_timestamp_seconds Unix time of next expected run
+# TYPE cronguard_next_expected_timestamp_seconds gauge
+cronguard_next_expected_timestamp_seconds{cronjob="demo-cj",name="demo",namespace="ns1"} %d
+# HELP cronguard_running_jobs Currently-running Jobs owned by the watched CronJob
+# TYPE cronguard_running_jobs gauge
+cronguard_running_jobs{cronjob="demo-cj",name="demo",namespace="ns1"} 1
+`, failure.Unix(), next.Unix())
+
+	if err := testutil.CollectAndCompare(c, strings.NewReader(expected),
+		"cronguard_last_duration_seconds",
+		"cronguard_last_failure_timestamp_seconds",
+		"cronguard_next_expected_timestamp_seconds",
+		"cronguard_running_jobs",
+	); err != nil {
+		t.Fatal(err)
+	}
+}
